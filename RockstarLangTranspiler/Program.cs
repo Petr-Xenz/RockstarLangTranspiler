c8550645c5f73bb06e4aa2ac501bc503b3fe4193
Пётр Михайлов <petr.xenz@gmail.com>
2020-08-02 18:06:30 +0300

    initial commit

---
+++ RockstarLangTranspiler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RockstarLangTranspiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
