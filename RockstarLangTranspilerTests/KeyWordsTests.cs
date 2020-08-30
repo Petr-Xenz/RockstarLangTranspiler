d320524d62c82f2a52c4e1d9ea34c226892a17b1
Пётр Михайлов <petr.xenz@gmail.com>
2020-08-30 17:26:20 +0300

    Проверка уникальности зарезервированных слов

---
+++ RockstarLangTranspilerTests/KeyWordsTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static RockstarLangTranspiler.KeyWords;
+
+namespace RockstarLangTranspilerTests
+{
+    [TestClass]
+    public class KeyWordsTests
+    {
+        [TestMethod]
+        public void AreAllKeyWordsUnique()
+        {
+            var repeated = new List<string>();
+            var checkSet = new HashSet<string>();
+
+            foreach (var word in AllReservedWords)
+            {
+                if (checkSet.Contains(word))
+                {
+                    repeated.Add(word);
+                    continue;
+                }
+
+                checkSet.Add(word);
+            }
+
+            if (repeated.Any())
+            {
+                throw new Exception(repeated.Aggregate((p, c) => $"{p}, {c}"));
+            }
+        }
+    }
+}
